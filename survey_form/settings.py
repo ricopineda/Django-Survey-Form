343dfadeaf941cedfcb6780254a2c0a665adb836
Ubuntu <ubuntu@ip-172-31-7-129.us-east-2.compute.internal>
2017-08-17 22:20:08 +0000

    deployed survey form

--- survey_form/settings.py
+++ survey_form/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!bb*jz3p7@lad1l!pi@^ke*x(@$sf-+^1xoj2^chy4v#g#sc#c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ec2-18-220-60-94.us-east-2.compute.amazonaws.com']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
